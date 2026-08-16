50346ba9950331da0a90114c99ad5a3b6f81ec17
Deiner Andrés De Luque Navarro <dadeluque@unicesar.edu.co>
2026-08-16 18:43:27 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/library/Main.java
+++ src/main/java/com/mycompany/library/Main.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.library;
 
-/**
- *
- * @author Deiner De Luque
- */
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    static ArrayList<Customer> customers = new ArrayList<>();
+    Scanner read = new Scanner(System.in);
+    
+    void createCustomer(){
+        String id, name, phone, email;
+        System.out.print("Digite la identificacion del cliente: ");
+        id = read.nextLine();
+        System.out.print("Digite el nombre del cliente: ");
+        name = read.nextLine();
+        System.out.print("Digite el telefono del cliente: ");
+        phone = read.nextLine();
+        System.out.print("Digite el email del cliente: ");
+        email = read.nextLine();
+        customers.add(new Customer(id, name, phone, email));
+        System.out.println("Cliente creado correctamente");
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
